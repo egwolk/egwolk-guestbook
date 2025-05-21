183ce0049d6169dd52caa847fc8733b0a581443d
egwolk <erincovacha@gmail.com>
2025-05-21 17:13:31 +0800

    feat: update project configuration and add utility functions for class management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="flex flex-col bg-background">
+        <button className='text-foreground' onClick={() => setCount((count) => count + 1)}>
           up
         </button>
-        <button onClick={() => setCount((count) => count - 1)}>
+        <button className='bg-secondary' onClick={() => setCount((count) => count - 1)}>
           down
         </button>
         <p>
