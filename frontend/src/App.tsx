bb2981fe77e4fd8c11d0d8e583f294c1099431b0
egwolk <erincovacha@gmail.com>
2025-05-21 18:03:15 +0800

    feat: implement card component for message display and update App component to utilize it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect  } from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+type Message = {
+  id: number
+  message: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState<Message[]>([])
+  useEffect(() => {
+    async function fetchMessages() {
+      const res = await fetch("/api/messages")
+      const data = await res.json()
+      setMessages(data.messages)
+    }
+    fetchMessages()
+  }, [])
 
   return (
     <>
-      <div className="flex flex-col bg-background">
-        <button className='text-foreground' onClick={() => setCount((count) => count + 1)}>
-          up
-        </button>
-        <button className='bg-secondary' onClick={() => setCount((count) => count - 1)}>
-          down
-        </button>
-        <p>
-          {count}
-        </p>
-      </div>
+      <Card className="w-[350px] m-auto">
+        <CardHeader>
+          <CardTitle>Messages</CardTitle>
+          <CardDescription>View All Messages</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ul>
+            {messages.map(msg => (
+              <li key={msg.id}>{msg.message}</li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
     </>
   )
 }
