0336fd3a9b51221ec006848800cedb79f2fa4650
egwolk <erincovacha@gmail.com>
2025-05-21 14:49:34 +0800

    feat: implement message validation and add delete route for messages

--- routes/messages.ts
+++ routes/messages.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { z } from "zod";
+import { zValidator } from '@hono/zod-validator';
 
 type Message = {
     id: number;
@@ -20,11 +21,29 @@ export const messagesRoute = new Hono()
 .get("/", (c) => {
     return c.json({messages: testMessages})
 })
-.post("/", async (c) => {
-    const data = await c.req.json()
+.post("/", zValidator("json", createMessageSchema), async (c) => {
+    const data = await c.req.valid("json")
     const message = createMessageSchema.parse(data)
     console.log(message)
+    testMessages.push({...message, id: testMessages.length + 1})
+    c.status(201)
     return c.json(message)
 })
-// .delete
+.get("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"))
+    const message = testMessages.find(message => message.id === id)
+    if (!message) {
+        return c.notFound()
+    }
+    return c.json({message}) 
+})
+.delete("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"))
+    const index = testMessages.findIndex(message => message.id === id)
+    if (index === -1) {
+        return c.notFound()
+    }
+    const deletedMessage = testMessages.splice(index, 1)[0]
+    return c.json({message: deletedMessage}) 
+})
 // .put
