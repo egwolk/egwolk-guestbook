c4b53868157b86bed48f174265fa4dc000e9c725
egwolk <erincovacha@gmail.com>
2025-05-24 16:55:36 +0800

    feat: implement UI components for CreateMessage route with Input, Label, and Button

--- frontend/src/routes/createMessage.tsx
+++ frontend/src/routes/createMessage.tsx
@@ -1,9 +1,22 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
 export const Route = createFileRoute('/createMessage')({
   component: CreateMessage,
 })
 
 function CreateMessage() {
-  return <div>Hello "/createMessage"!</div>
+  return (
+    <div className="p-2">
+      <h2>Send Message</h2>
+      <form action="" className='max-w-xl m-auto'>
+        <Label className='mb-1' htmlFor="message">Message</Label>
+        <Input type="text" id="message" placeholder="message" />
+        <Button className='mt-4' type='submit'>Send</Button>
+      </form>
+    </div>
+  )
 }
