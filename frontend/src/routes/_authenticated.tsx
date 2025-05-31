1bb28ca8159c4332f5872cbdb5dfea91e1becce0
egwolk <erincovacha@gmail.com>
2025-05-31 17:49:37 +0800

    feat: implement user query fetching in authenticated route for improved user context handling

--- frontend/src/routes/_authenticated.tsx
+++ frontend/src/routes/_authenticated.tsx
@@ -1,10 +1,14 @@
+import { userQueryOptions } from '@/lib/api'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 // import { userQueryOptions } from '@/lib/api'
 
 
 const Login = () => {
     return (
-        <div>You gotta login</div>
+        <>
+          <div>You gotta login</div>
+          <a href="/api/login">Login</a>
+        </>
     )
 }
 
@@ -21,9 +25,16 @@ const Component = () => {
 
 // src/routes/_authenticated.tsx
 export const Route = createFileRoute('/_authenticated')({
-  beforeLoad: async () => {
-    // return {user: {name: ""}}
-    return {user: null} // Simulating no user logged in
+  beforeLoad: async ({ context }) => {
+    const queryCLient = context.queryClient
+    try {
+      const data = await queryCLient.fetchQuery(userQueryOptions)
+      return data
+    } catch (e) {
+      console.error(e)
+      return {user: null}
+    }
+    // return {user: null} // Simulating no user logged in
   },
   component: Component
 })
