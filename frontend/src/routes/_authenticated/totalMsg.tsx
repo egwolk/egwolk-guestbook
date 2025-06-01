bceba071f4b3f74c044171fc49f08a3a402d3895
egwolk <erincovacha@gmail.com>
2025-06-01 19:01:11 +0800

    feat: add totalMsg route with message retrieval and display functionality

---
+++ frontend/src/routes/_authenticated/totalMsg.tsx
@@ -0,0 +1,52 @@
+// for testing purposes
+import { createFileRoute } from '@tanstack/react-router'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { useQuery } from '@tanstack/react-query'
+import { api } from '@/lib/api'
+
+export const Route = createFileRoute('/_authenticated/totalMsg')({
+  component: TestTSQ,
+})
+
+async function getTotalMessages() {
+  const res = await api.messages['total-messages'].$get()
+  if (!res.ok) {
+    throw new Error('Network response was not ok')
+  }
+  const data = await res.json()
+  return data
+} 
+
+function TestTSQ() {
+  const { isPending, error, data } = useQuery({ 
+    queryKey: ['get-total-messages'], 
+    queryFn: getTotalMessages 
+  })
+
+  // if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  return (
+    <>
+      <Card className="w-[350px] m-auto">
+        <CardHeader>
+          <CardTitle>Messages</CardTitle>
+          <CardDescription>View All Messages</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {isPending ? "..." : data.total}
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+
