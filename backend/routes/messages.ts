620f49c976b8bf24a050f4bdb6bec5d0f041cd48
egwolk <erincovacha@gmail.com>
2025-06-01 18:58:54 +0800

    feat: implement CRUD operations for messages using drizzle ORM

--- backend/routes/messages.ts
+++ backend/routes/messages.ts
@@ -4,54 +4,102 @@ import { zValidator } from '@hono/zod-validator';
 
 import { getUser } from "../kinde";
 
+import { db } from "../db"
+import { messages as messagesTable } from "../db/schema/messages"
+import { eq, desc, count, and } from "drizzle-orm"
+
 const messageSchema = z.object({
     id: z.number().int().positive().min(1),
     message: z.string().min(1).max(100),
 })
 
-const testMessages: Message[] = [
-    { id: 1, message: "Hello, world!" },
-    { id: 2, message: "Hono is great!" },
-    { id: 3, message: "TypeScript is awesome!" },
-]
+// const testMessages: Message[] = [
+//     { id: 1, message: "Hello, world!" },
+//     { id: 2, message: "Hono is great!" },
+//     { id: 3, message: "TypeScript is awesome!" },
+// ]
 
 type Message = z.infer<typeof messageSchema>
 
 const createPostSchema = messageSchema.omit({ id: true })
 
 export const messagesRoute = new Hono()
-.get("/", getUser, (c) => {
+.get("/", getUser, async (c) => {
     const user = c.var.user
-    return c.json({messages: testMessages})
+    const messages = await db
+        .select()
+        .from(messagesTable)
+        .where(eq(messagesTable.userId, user.id))
+        .orderBy(desc(messagesTable.createdAt))
+        .limit(100)
+    return c.json({messages: messages})
 })
 .post("/",getUser, zValidator("json", createPostSchema), async (c) => {
-    const data = await c.req.valid("json")
-    const message = createPostSchema.parse(data)
+    const message = await c.req.valid("json")
+    const user = c.var.user
+
+    const result = await db.insert(messagesTable).values({
+        ...message,
+        userId: user.id
+    }).returning()
+
+    // const message = createPostSchema.parse(data)
     console.log(message)
-    testMessages.push({...message, id: testMessages.length + 1})
+    // testMessages.push({...message, id: testMessages.length + 1}) 
     c.status(201)
-    return c.json(message)
+    return c.json(result)
 })
-.get("/total-messages", async (c) => { // for testing
-    await new Promise(resolve => setTimeout(resolve, 3000)) // Simulate a delay
-    const total = testMessages.length
-    return c.json({total})
+.get("/total-messages", getUser, async (c) => { // for testing
+    // await new Promise(resolve => setTimeout(resolve, 3000)) // Simulate a delay
+    // const total = testMessages.length
+    // return c.json({total})
+
+    const user = c.var.user
+    const totalResult = await db
+        .select({ total: count() })
+        .from(messagesTable)
+        .where(eq(messagesTable.userId, user.id))
+        .limit(1)
+        .then(res => res[0])
+
+    return c.json(totalResult);
 })
-.get("/:id{[0-9]+}", getUser, (c) => {
+.get("/:id{[0-9]+}", getUser, async (c) => {
     const id = Number.parseInt(c.req.param("id"))
-    const message = testMessages.find(message => message.id === id)
+    const user = c.var.user
+
+    const message = await db
+        .select()
+        .from(messagesTable)
+        .where(and(eq(messagesTable.userId, user.id), eq(messagesTable.id, id)))
+        .then(res => res[0])
+
+    // const message = testMessages.find(message => message.id === id)
+
     if (!message) {
         return c.notFound()
     }
     return c.json({message}) 
 })
-.delete("/:id{[0-9]+}", getUser, (c) => {
+.delete("/:id{[0-9]+}", getUser, async (c) => {
     const id = Number.parseInt(c.req.param("id"))
-    const index = testMessages.findIndex(message => message.id === id)
-    if (index === -1) {
+    // const index = testMessages.findIndex(message => message.id === id)
+    const user = c.var.user
+
+    const message = await db
+        .delete(messagesTable)
+        .where(and(eq(messagesTable.userId, user.id), eq(messagesTable.id, id)))
+        .returning()
+        .then(res => res[0])
+
+
+    // if (index === -1) {
+    //     return c.notFound()
+    // }
+    if (!message) {
         return c.notFound()
     }
-    const deletedMessage = testMessages.splice(index, 1)[0]
-    return c.json({message: deletedMessage}) 
+    // const deletedMessage = testMessages.splice(index, 1)[0]
+    return c.json({message: message}) 
 })
 // .put
