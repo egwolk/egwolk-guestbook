f9df5e2e2b05065b3e7332e8ed335f18564c57e5
egwolk <erincovacha@gmail.com>
2025-06-03 23:22:56 +0800

    feat: refactor createMessageSchema to use insertMessagesSchema for improved validation

--- backend/sharedTypes.ts
+++ backend/sharedTypes.ts
@@ -1,10 +1,16 @@
 import { z } from "zod";
+import {insertMessagesSchema} from "./db/schema/messages";
 
-export const messageSchema = z.object({
-    id: z.number().int().positive().min(1),
-    message: z.string().min(1).max(100),
+export const createMessageSchema = insertMessagesSchema.omit({ 
+    id: true, 
+    userId: true,
+    createdAt: true
 })
+// z.object({
+//     id: z.number().int().positive().min(1),
+//     message: z.string().min(1).max(100),
+// })
 
 // type Message = z.infer<typeof messageSchema>
 
-export const createMessageSchema = messageSchema.omit({ id: true })
+// export const createMessageSchema = messageSchema.omit({ id: true })
