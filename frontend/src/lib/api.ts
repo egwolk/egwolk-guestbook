21d1bd403c08a48ee52532e74e8881886bef74f8
egwolk <erincovacha@gmail.com>
2025-06-06 21:32:37 +0800

    feat: implement deleteMessage function with simulated network delay

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -41,6 +41,7 @@ export const getAllMessagesQueryOptions = queryOptions({
 
 export async function createMessage({ value }: { value: CreateMessage }) {
   await new Promise(r => setTimeout(r, 3000)) // Simulate network delay
+  //throw new Error('Server Error') // for testing
   const res = await api.messages.$post({ json: value })
       if (!res.ok) {
         throw new Error('server error')
@@ -56,3 +57,13 @@ export const  loadingCreateMessageQueryOptions = queryOptions<{message?: CreateM
     },
     staleTime: Infinity,
 }) 
+
+export async function deleteMessage({ id }: { id: number }) {
+  await new Promise(r => setTimeout(r, 3000)) // Simulate network delay
+  const res = await api.messages[":id{[0-9]+}"].$delete({ 
+    param: { id: id.toString() } 
+  })
+  if (!res.ok) {
+    throw new Error('Server Error')
+  }
+}
